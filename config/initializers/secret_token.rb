57dabb2d59aefbaebb115507e5b9c755602ad048
Ryan Ahearn <ryan@coshx.com>
2011-10-25 09:22:29 -0400

    Adds skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoggingBugExample::Application.config.secret_token = 'b4e39a3f4521d28e26495a69057f92aa7e4d10f35e0025ba16daa718d841bf9e2d8d8a35e4a89dda80fdf624455f5b11cc3d7c8b97446284f1691f911122a2af'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+LoggingBugExample::Application.config.session_store :cookie_store, :key => '_logging-bug-example_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# LoggingBugExample::Application.config.session_store :active_record_store
